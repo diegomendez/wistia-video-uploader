3aafa9f18505c4b62e744b37ddfa3009b99bb81f
Diego Mendez <diego.m@wideo.co>
2017-06-01 14:05:00 +0200

    application app

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');  
+var app = express();  
+  
+app.use(express.static("App"));  
+  
+app.get('/', function (req, res) {  
+    res.redirect('/');  
+});  
+  
+app.listen(8080, 'localhost');  
+console.log("WistiaUploader Server is Listening on port 8080"); 
