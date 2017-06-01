b2a98d2ffc16ebfcf8a9eafefd5688c20d264e3d
Diego Mendez <diego.m@wideo.co>
2017-06-01 15:34:32 +0200

    update port

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ var app = express();
   
 app.use(express.static("App"));  
   
-app.get('/', function (req, res) {  
-    res.redirect('/');  
-});  
   
 app.listen(process.env.PORT || 8080, function() {
 console.log("WistiaUploader Server is running on port %d in %s mode"); 
